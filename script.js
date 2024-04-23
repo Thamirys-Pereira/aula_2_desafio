2edf87f71f4127b22151046d6f3d5b9a8e4724b9
Thamirys Regina <theatrebispo@gmail.com>
2024-04-23 19:09:22 -0300

    aula2-desafio

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var nome = prompt("Insira seu nome");
+var idade = prompt("Insira sua idade");
+
+var numb = parseInt(idade);
+
+var resultadoA = "Sejá bem vindo(a)," + " " + nome + "!";
+alert(resultadoA);
+
+var resultadoB = 2024 - numb;
+console.log(resultadoB);
+
+
+
